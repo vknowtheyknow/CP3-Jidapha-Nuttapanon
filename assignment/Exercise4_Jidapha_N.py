bca6cd9bdbf176decf31da1347a9e28d47222494
justlikedev <vknowtheyknow@gmail.com>
2020-06-24 11:47:34 +0700

    ex4

---
+++ assignment/Exercise4_Jidapha_N.py
@@ -0,0 +1,10 @@
+Fe =80
+Gb =76.33
+ICS = 56
+Cp = 62
+
+print("--- Your Score ---")
+print("Foundation English : ",Fe)
+print("General Business : ",Gb)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",Cp)
